b59064a7a7012ab3b791d0a745a5355969b6b59c
alxwuz <alex.wu24@education.nsw.gov.au>
2025-03-13 14:38:05 +1100

    Final
    All done?

---
+++ calculator.py
@@ -0,0 +1,6 @@
+from math_operations import add_numbers
+
+a = int(input("Enter your first number: "))
+b = int(input("Enter your second number: "))
+
+print(add_numbers(a,b))
---
+++ math_operations.py
@@ -0,0 +1,2 @@
+def add_numbers(a,b):
+    return(f"The sum of your numbers is {a+b}.")
